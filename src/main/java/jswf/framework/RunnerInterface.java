a9b88ed8dbbac44faa78b893523bdc534b9431b2
odiel <odiel.leon@juntacorp.com>
2016-11-14 14:04:08 -0500

    Added comments

--- src/main/java/jswf/framework/RunnerInterface.java
+++ src/main/java/jswf/framework/RunnerInterface.java
@@ -2,8 +2,19 @@ package jswf.framework;
 
 import java.util.HashMap;
 
+/**
+ * Interface to define a runner type of object to be passed to the framework
+ */
 public interface RunnerInterface {
 
+    /**
+     * Method to run the pipeline flow by invoking the components
+     *
+     * @param component First component in the pipeline.
+     *                  The implementation should take care of calling at some point component.invoke(environment)
+     * @param services Collection of defined services in the Framework
+     * @throws Exception
+     */
     public void run(ComponentInterface component, HashMap<String, Object> services) throws Exception;
 
 }
